5659f280faee9387f539fa3a40c4ef800ea4a9ad
jaswanth0202reddy <140830503+jaswanth0202reddy@users.noreply.github.com>
2025-04-18 02:08:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def index():
     return render_template('index.html', converted_amount=converted_amount, error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=8080,debug=True)
